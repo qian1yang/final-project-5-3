6d8412a7db1d6327d3c3d6e511e953c9be0cd326
kevinqian <qianyiyang2004@icloud.com>
2022-05-01 11:47:50 -0400

    123

--- final project/ViewController.swift
+++ final project/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    
+    @IBOutlet weak var entryDollarTextField: UITextField!
+    
+   
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var buttonClickedLabel: UILabel!
+    
+    var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func convert(_ sender: UIButton) {
+        if let result = entryDollarTextField.text{
+            if (result == ""){
+                answerLabel.text = String(3)
+            }
+            else  {
+                if let num = Double(result) {
+                    let answer = num * 6.6
+                    answerLabel.text = String(answer)
+                }
+            }
+    }
+       
+    }
+}
 
+func calcAnswer (num: Double) -> Double {
+    
+    return num * 6.6
 }
 
